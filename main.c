41472e19bf288bdcfadd00b8234f6edef368ecfc
mindsgn <mindsgn@mindsgn.co.za>
2020-06-26 00:25:41 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,26 @@
+/*
+* beat machine sequencer by sibongiseni tembe seni.tembe@protonmail.com
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+
+long double milliseconds = -1;
+float seconds = -1;
+float beats = 0;
+float beat_time = 0;
+int channel = 0;
+
+
+
+int main(){
+	while(1){
+		
+		++milliseconds;
+	}
+
+	return 0;
+}
+
+void log(){	
+}
